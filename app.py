fabeeff2a273142fd6fd6d6329a715de27149fbe
Ashutosh Pranjal <pranjalashutosh09@gmail.com>
2024-03-05 20:44:35 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -36,5 +36,22 @@ def delete(sno):
     db.session.commit()
     return redirect("/")
 
+@app.route('/update/<int:sno>',methods=['GET','POST'])
+def update(sno):
+     if request.method=="POST":
+        title=request.form['title']
+        desc1=request.form['desc']
+        todo=Todo.query.filter_by(sno=sno).first()
+        todo.title=title
+        todo.desc=desc1
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+     
+     todo=Todo.query.filter_by(sno=sno).first()
+     return render_template('update.html', todo=todo)
+    
+    
+
 if __name__=='__main__':
     app.run(debug=True)
